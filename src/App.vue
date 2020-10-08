83d16ed287d135c8f974bd94da7d5efd7256eac1
Dries Goethuys <dries.goethuys@gmail.com>
2020-10-08 03:52:59 +0200

    personal information and links

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Logo" src="./assets/logo.png">
+    <PersonalInfo />
+    <Links />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PersonalInfo from './components/PersonalInfo.vue'
+import Links from './components/Links.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PersonalInfo,
+    Links
   }
 }
 </script>
 
 <style>
+body {
+
+  background-color: #000000;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffff;
+  background-color: #000000;
   margin-top: 60px;
+}
+a {
+  color: #9b9b9b;
 }
 </style>
---
+++ src/components/PersonalInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="personal">
+    <h3>Dries Goethuys</h3>
+    <ul>
+      <li>Violetstraat 17</li>
+      <li>Tienen, Belgium</li>
+      <li><a href="mailto:dries.goethuys@gmail.com">dries.goethuys@gmail.com</a></li>
+      <li><a href="tel:+32478703934">+32478 70 39 34</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalInfo'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.personal {
+  margin: 0 10em;
+  text-align: left;
+}
+ul {
+  list-style-type: none;
+}
+</style>
